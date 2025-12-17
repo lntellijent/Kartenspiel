7b75bcf3bcb579cebe73da4fe229799b8c303c20
lntellijent <inf25147@lehre.dhbw-stuttgart.de>
2025-12-17 10:55:57 +0100

    Cards + Deck implemented + Output

--- card.c
+++ card.c
@@ -2,3 +2,4 @@
 // Created by hoerz on 15.12.2025.
 //
 
+#include "card.h"
--- main.c
+++ main.c
@@ -3,11 +3,26 @@
 #include "card.h"
 //C-Level C11
 
+static char getSuit(Suit s) {
+    switch (s) {
+        case 0:
+            return 'S';
+        case 1:
+            return 'C';
+        case 2:
+            return 'H';
+        case 3:
+            return 'D';
+        default:
+            return 'N';
+    }
+}
+
 int main(void) {
     Deck* d = deck_create_standard();
     Card c;
     while (deck_draw_top(d, &c) == 0) {
-        printf("gezogene Karte: %u/%u\n",c.suit,c.rank);
+        printf("gezogene Karte: %c/%u\n",getSuit(c.suit),c.rank);
     }
     return 0;
 }
